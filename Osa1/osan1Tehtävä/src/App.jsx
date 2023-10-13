c9fb6504a1c46d804ed5f3f6e570d35db36c03e4
Eetu Valkamo <eetu.valkamo@edu.turkuamk.fi>
2023-10-13 14:54:07 +0300

    osa 1 -> 1.8 valmis

--- Osa1/osan1Tehtävä/src/App.jsx
+++ Osa1/osan1Tehtävä/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react"
 
-const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+const Statistics = (props) => {
+    const good = props.good
+    const neutral = props.neutral
+    const bad = props.bad
 
     const all = good + neutral + bad
 
@@ -11,16 +11,11 @@ const App = () => {
     const avarage = (good * 1 + bad * -1) / all
     const roundedAvarage = avarage.toFixed(2)
 
-    const positive = good / all
+    const positive = 100 * (good / all)
     const roundedPositive = positive.toFixed(2)
 
     return (
         <div>
-            <h1>Give feedback</h1>
-            <button onClick={() => setGood(good + 1)}>Good</button>
-            <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-            <button onClick={() => setBad(bad + 1)}>Bad</button>
-
             <h2>Statistics</h2>
             <p>Good: {good}</p>
             <p>Neutral: {neutral}</p>
@@ -31,6 +26,23 @@ const App = () => {
             <p>All: {all}</p>
             <p>Avarage: {roundedAvarage}</p>
             <p>Positive: {roundedPositive}%</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <button onClick={() => setGood(good + 1)}>Good</button>
+            <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+            <button onClick={() => setBad(bad + 1)}>Bad</button>
+
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
