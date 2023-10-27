82ef23f603b5f55eff782a998fcb5b85a80b772c
Eetu Valkamo <eetu.valkamo@edu.turkuamk.fi>
2023-10-27 16:12:32 +0300

    Osa 2 -> 2.5 siirretty kurssitiedot

---
+++ Osa2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+
+const Header = (props) => {
+    return <h2>{props.course.name}</h2>
+}
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
+    )
+}
+
+const Content = (props) => {
+    console.log("Content props: ", props)
+    return (
+        <div>
+            {props.course.parts.map((part) => (
+                <Part key={part.id} part={part} />
+            ))}
+        </div>
+    )
+}
+
+const Total = (props) => {
+    // Makes a list of the exercises | [10, 7, 14]
+    const parts = props.course.parts.map((course) => course.exercises)
+    // Adds each part in the list to the sum | 31
+    const numberOfExercises = parts.reduce((sum, parts) => sum + parts)
+
+    return <strong>Number of exercises {numberOfExercises}</strong>
+}
+
+const Course = (props) => {
+    console.log("Course toimii...")
+
+    return (
+        <div>
+            <Header course={props.course} />
+            <Content course={props.course} />
+            <Total course={props.course} />
+        </div>
+    )
+}
+
+export default Course
