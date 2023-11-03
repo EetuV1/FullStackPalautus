e05cca8aef1462d7806340096cf58d95af414d9b
Eetu Valkamo <eetu.valkamo@edu.turkuamk.fi>
2023-11-03 17:26:25 +0200

    Osa 2 -> 2.9 valmis

--- Osa2/osan2pohja/src/App.jsx
+++ Osa2/osan2pohja/src/App.jsx
@@ -3,7 +3,14 @@ import { useState } from "react"
 const App = () => {
     const [persons, setPersons] = useState([
         { name: "Arto Hellas", number: "045-1234567" },
+        { name: "Ada Lovelace", number: "39-44-5323523" },
+        { name: "Dan Abramov", number: "12-43-234345" },
+        { name: "Mary Poppendieck", number: "39-23-6423122" },
     ])
+    // So we can show the searched persons without modifying the original persons array
+    const [searchPersons, setSearchPersons] = useState([...persons])
+    const [searchInput, setSearchInput] = useState("")
+
     const [newName, setNewName] = useState("")
     const [newNumber, setNewNumber] = useState("")
 
@@ -18,6 +25,10 @@ const App = () => {
             alert(`${newName} is already added to phonebook.`)
         } else {
             setPersons([...persons, { name: newName, number: newNumber }])
+            setSearchPersons([
+                ...searchPersons,
+                { name: newName, number: newNumber },
+            ])
         }
 
         // Clear inputs
@@ -28,16 +39,31 @@ const App = () => {
     const personsToShow =
         persons.length == 0
             ? "There Are No Contacts"
-            : persons.map((person) => (
+            : searchPersons.map((person) => (
                   <li key={person.name}>
                       {person.name} {person.number}
                   </li>
               ))
 
+    const handleSeachInputChange = (event) => {
+        setSearchInput(event.target.value)
+        const searchInputLowerCase = event.target.value.toLowerCase()
+        const personsToShow = persons.filter((person) =>
+            person.name.toLowerCase().includes(searchInputLowerCase)
+        )
+        setSearchPersons(personsToShow)
+    }
+
     return (
         <div>
             <h3>Phonebook</h3>
-
+            Filter shown with:
+            <input
+                id="searchInput"
+                value={searchInput}
+                onChange={handleSeachInputChange}
+            ></input>
+            <h3>Add a new</h3>
             <form onSubmit={formSubmit}>
                 <div>
                     Name:
@@ -58,9 +84,7 @@ const App = () => {
                 <br />
                 <button type="submit">Add</button>
             </form>
-
             <h3>Numbers</h3>
-
             <ul>{personsToShow}</ul>
         </div>
     )
