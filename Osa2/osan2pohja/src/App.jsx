7172ef807fddc26225f2cc8f0c0790ff03d5ec2d
Eetu Valkamo <eetu.valkamo@edu.turkuamk.fi>
2023-11-03 16:47:10 +0200

    Osa 2 -> 2.8 valmis

--- Osa2/osan2pohja/src/App.jsx
+++ Osa2/osan2pohja/src/App.jsx
@@ -1,45 +1,66 @@
 import { useState } from "react"
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "045-1234567" },
+    ])
     const [newName, setNewName] = useState("")
-
-    function checkName(name) {
-        const nameAlreadyExists = persons.some((person) => person.name === name)
-        if (nameAlreadyExists) {
-            alert(`${name} is already added to phonebook.`)
-        } else {
-            setPersons([...persons, { name }])
-        }
-    }
+    const [newNumber, setNewNumber] = useState("")
 
     const formSubmit = (event) => {
         event.preventDefault()
-        checkName(newName)
+
+        // Check if name already exists
+        const nameAlreadyExists = persons.some(
+            (person) => person.name === newName
+        )
+        if (nameAlreadyExists) {
+            alert(`${newName} is already added to phonebook.`)
+        } else {
+            setPersons([...persons, { name: newName, number: newNumber }])
+        }
+
+        // Clear inputs
         setNewName("")
+        setNewNumber("")
     }
 
     const personsToShow =
         persons.length == 0
             ? "There Are No Contacts"
-            : persons.map((person) => <li key={person.name}>{person.name}</li>)
+            : persons.map((person) => (
+                  <li key={person.name}>
+                      {person.name} {person.number}
+                  </li>
+              ))
 
     return (
         <div>
             <h3>Phonebook</h3>
 
             <form onSubmit={formSubmit}>
-                Name :
-                <input
-                    id="name"
-                    value={newName}
-                    onChange={(event) => setNewName(event.target.value)}
-                />
+                <div>
+                    Name:
+                    <input
+                        id="name"
+                        value={newName}
+                        onChange={(event) => setNewName(event.target.value)}
+                    />
+                </div>
+                <div>
+                    Number:
+                    <input
+                        id="number"
+                        value={newNumber}
+                        onChange={(event) => setNewNumber(event.target.value)}
+                    />
+                </div>
                 <br />
                 <button type="submit">Add</button>
             </form>
 
             <h3>Numbers</h3>
+
             <ul>{personsToShow}</ul>
         </div>
     )
