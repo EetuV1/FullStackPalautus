6c4c00263536e460380359f9ce047c329fa63035
Eetu Valkamo <eetu.valkamo@gmail.com>
2025-03-12 13:35:14 +0200

    4.10 valmis

--- Osa4/tests/blogs_api.test.js
+++ Osa4/tests/blogs_api.test.js
@@ -12,12 +12,6 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/)
 })
 
-test("there is one blog", async () => {
-  const response = await api.get("/api/blogs")
-
-  assert.strictEqual(response.body.length, 1)
-})
-
 test("the first blog's title is *this is title*", async () => {
   const response = await api.get("/api/blogs")
 
@@ -31,6 +25,28 @@ test("all blogs have an id property", async () => {
   response.body.forEach((blog) => {
     assert.strictEqual(blog.id !== undefined, true)
   })
+})
+
+test("blogs can be added with a post request", async () => {
+  const newBlog = {
+    title: "new blog",
+    author: "new author",
+    url: "new url",
+    likes: 0,
+  }
+
+  const responseAtBefore = await api.get("/api/blogs")
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/)
+
+  const responseAtEnd = await api.get("/api/blogs")
+  const difference = responseAtEnd.body.length - responseAtBefore.body.length
+
+  assert.strictEqual(difference, 1)
 })
 
 after(async () => {
